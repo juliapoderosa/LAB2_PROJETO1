c53881822703c1de4e7ae60aa57d983ec3aaa6d8
Julia Ribeiro Costa <C3007892@fgv.edu.br>
2025-08-12 16:26:43 -0300

    3/4: função que sorteia 7 números e calcula média

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ import random
 def cumprimento (texto): 
     return f"ola,{texto}"
 
-if __name__ == "_main_": 
+def media_7_numeros(): 
+     nums = [random.randint(1100) for _ in range(7)]
+    media = sum(nums)/ len(nums)
+    return nums, media 
+
+if __ name__ == "_main_": 
      nome_comleto = "Julia Ribeiro Costa"
+
      print(cumprimento(nome_completo))
+     
+     nums, media = media_7_numeros()
+     print("Numeros sorteados:",nums)
+     print("Média:", media)
     
 
 
