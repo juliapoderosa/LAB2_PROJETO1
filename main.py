2b0e842bf7b1fb6b05aeed6fd524ac55c226729f
Julia Ribeiro Costa <C3007892@fgv.edu.br>
2025-08-12 16:15:45 -0300

    função cumprimento e chamada com nome

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("HELLO WORD")
+import random
+
+def cumprimento (texto): 
+    return f"ola,{texto}"
+
+if __name__ == "_main_": 
+     nome_comleto = "Julia Ribeiro Costa"
+     print(cumprimento(nome_completo))
+    
+
+
+
